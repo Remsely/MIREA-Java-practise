aa0fd7fd2146f01eb3d1c30546264200ec136d20
Remsely <prokopchukromain@mail.ru>
2023-10-02 21:45:25 +0300

    Practise5 task4

--- src/practise5/Task4/Animation.java
+++ src/practise5/Task4/Animation.java
@@ -6,16 +6,13 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
 public class Animation extends JPanel implements ActionListener {
     private final BufferedImage[] animationImages;
     private final int width;
     private final int height;
     private int currentFrame;
-    private final Timer timer;
 
     public Animation(String[] imagePaths, int width, int height, int frameDelay){
         animationImages = new BufferedImage[imagePaths.length];
@@ -23,14 +20,14 @@ public class Animation extends JPanel implements ActionListener {
         this.width = width;
         for (int i = 0; i < imagePaths.length; i++){
             try{
-                animationImages[i] = ImageIO.read(new File(imagePaths[0]));
+                animationImages[i] = ImageIO.read(new File(imagePaths[i]));
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
         }
 
         int initialDelay = 0;
-        timer = new Timer(frameDelay, this);
+        Timer timer = new Timer(frameDelay, this);
         timer.setInitialDelay(initialDelay);
         timer.start();
     }
@@ -49,23 +46,30 @@ public class Animation extends JPanel implements ActionListener {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int animationLength = 20;
-        String[] imagePaths = new String[animationLength];
-        for (int i = 0 ; i < animationLength; i++){
-            imagePaths[i] = scanner.nextLine();
+        String fileName = "src\\practise5\\Task4\\config.txt";
+
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            int width = Integer.parseInt(br.readLine());
+            int height = Integer.parseInt(br.readLine());
+            int animationLength = Integer.parseInt(br.readLine());
+            int delay = Integer.parseInt(br.readLine());
+            String[] imagePaths = new String[animationLength];
+
+            for (int i = 0; i < animationLength; i++)
+                imagePaths[i] = br.readLine();
+
+            SwingUtilities.invokeLater(() -> {
+                JFrame frame = new JFrame("Анимация из картинок");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                Animation animationPanel = new Animation(imagePaths, width, height, delay);
+                frame.add(animationPanel);
+
+                frame.setSize(width + 40, height + 40);
+                frame.setVisible(true);
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        int frameDelay = Integer.parseInt(scanner.nextLine());
-
-        SwingUtilities.invokeLater(() -> {
-            JFrame frame = new JFrame("Анимация из картинок");
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-            Animation animationPanel = new Animation(imagePaths, 800, 600, frameDelay);
-            frame.add(animationPanel);
-
-            frame.pack();
-            frame.setVisible(true);
-        });
     }
 }
